e8cce232dae2ef886bba0e5f0e1297d1f2448101
RolandVrignon <roland.vrignon@gmail.com>
2022-09-20 16:09:08 +0200

    minishell

--- src/init/utils.c
+++ src/init/utils.c
@@ -6,7 +6,7 @@
 /*   By: rvrignon <rvrignon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 15:29:44 by rvrignon          #+#    #+#             */
-/*   Updated: 2022/09/20 15:38:11 by rvrignon         ###   ########.fr       */
+/*   Updated: 2022/09/20 16:09:06 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ t_data *set_data(char **env)
 		return (NULL);
 	data->env = env;
 	data->line = NULL;
-	data->parsing = NULL;
 	data->pipex = NULL;
 	data->lexer = NULL;
 	return (data);
