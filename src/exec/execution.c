0920e79b4aaeef7c2846cc461eea57ff12245def
RolandVrignon <roland.vrignon@gmail.com>
2022-09-30 14:23:09 +0200

    minishell

--- src/exec/execution.c
+++ src/exec/execution.c
@@ -6,7 +6,7 @@
 /*   By: rvrignon <rvrignon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 19:25:10 by rvrignon          #+#    #+#             */
-/*   Updated: 2022/09/30 14:12:39 by rvrignon         ###   ########.fr       */
+/*   Updated: 2022/09/30 14:23:03 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	child_process(t_data *data, int cmd)
 {
 	if (handle_fd(data, cmd))
 	{
-		dprintf(2, "CMD %d || FD[0] = %d && FD[1] = %d && oldfd = %d\n", cmd, data->fd[0], data->fd[1], data->oldfd);
+		// dprintf(2, "CMD %d || FD[0] = %d && FD[1] = %d && oldfd = %d\n", cmd, data->fd[0], data->fd[1], data->oldfd);
 		execute(data, cmd);
 	}
 }
